93fc8b9c623872f4eafdabb332f5c051e28a8c9a
0xFloop <floopcrypto@yahoo.com>
2023-03-07 09:46:28 -0500

    test

--- apps/REDACTED_SERVER/src/index.ts
+++ apps/REDACTED_SERVER/src/index.ts
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
 
 app.post("/payload", (req, res) => {
   console.log("POST REQ FROM GITHUB BELOW: \n");
-  console.log(JSON.stringify(req.body));
+  console.log(req.body);
   console.log(req);
   res.send("POST REQ FROM GITHUB BELOW: \n" + JSON.stringify(req.body));
 });
