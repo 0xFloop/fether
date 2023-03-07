896a0a85ad301023245136a218ff9cb0b1b8c412
0xFloop <floopcrypto@yahoo.com>
2023-03-07 14:50:06 -0500

    add body-parser

--- apps/REDACTED_SERVER/src/index.ts
+++ apps/REDACTED_SERVER/src/index.ts
@@ -1,15 +1,19 @@
 import express from "express";
 const app = express();
+var bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
+
 const port = 3001;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/payload", (req, res) => {
+app.post("/payload", jsonParser, (req, res) => {
   //@ts-ignore
 
   console.log("POST REQ FROM GITHUB BELOW: \n" + JSON.stringify(req.payload));
+  console.log("POST REQ FROM GITHUB BELOW: \n" + JSON.stringify(req.body));
   // res.send("POST REQ FROM GITHUB BELOW: \n" + JSON.stringify(req.body));
   //@ts-ignore
   res.send("POST REQ FROM GITHUB BELOW : \n" + JSON.stringify(req.payload));
