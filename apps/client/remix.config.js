fb85ecb4b4da4838bf1174921443477e459f3729
0xFloop <floopcrypto@yahoo.com>
2023-03-13 22:14:16 -0400

    config update

--- apps/client/remix.config.js
+++ apps/client/remix.config.js
@@ -4,15 +4,9 @@ module.exports = {
     v2_routeConvention: true,
     unstable_tailwind: true,
   },
+  cacheDirectory: "./node_modules/.cache/remix",
   serverBuildTarget: "vercel",
-
   ignoredRouteFiles: ["**/.*"],
-  // When running locally in development mode, we use the built-in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   serverBuildPath: "api/index.js",
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
 };
