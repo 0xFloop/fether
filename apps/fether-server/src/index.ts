c055fa2ea290f0f1c1faaf847395377844df17d7
0xFloop <floopcrypto@yahoo.com>
2023-03-14 16:54:53 -0400

    pk formatting

--- apps/fether-server/src/index.ts
+++ apps/fether-server/src/index.ts
@@ -12,7 +12,7 @@ const pk = process.env.appPK as string;
 
 const formattedPk = pk.replace(/\\n/g, "\n");
 
-const octo = new Octo({ appId: "302483", privateKey: formattedPk });
+const octo = new Octo({ appId: "302483", privateKey: pk });
 
 app.get("/", (req, res) => {
   res.send("Hello Wor ld!");
