39dd0a239ef80ae4dcce911014b8bf8be4e0de39
0xFloop <floopcrypto@yahoo.com>
2023-03-14 12:22:37 -0400

    style for mobile

--- apps/client/app/routes/_index.tsx
+++ apps/client/app/routes/_index.tsx
@@ -3,12 +3,12 @@ import Typewriter from "typewriter-effect";
 export default function Index() {
   return (
     <div className="w-screen h-screen overflow-hidden">
-      <div className="m-auto relative flex flex-col justify-center items-center w-full h-full">
+      <div className="m-auto relative flex flex-col justify-center items-center p-6 text-center w-full h-full">
         <div className="font-display flex flex-row text-[150px] absolute  bottom-4 left-6 align-bottom items-baseline">
-          <h1 className="text-[150px] leading-[120px]">fether</h1>
-          <p className="text-2xl">by floop</p>
+          <h1 className="text-[100px] leading-[80px] md:text-[150px] md:leading-[120px]">fether</h1>
+          <p className="text-base md:text-2xl">by floop</p>
         </div>
-        <p className="font-sans text-2xl inline-block">
+        <p className="font-sans text-base md:text-2xl inline-block">
           <Typewriter
             options={{
               strings: [
@@ -23,15 +23,6 @@ export default function Index() {
             }}
           />{" "}
         </p>
-
-        {/* <div className="flex flex-row absolute bottom-4 right-6 border text-2xl">
-          <a href="" className="">
-            contact
-          </a>
-          <a href="" className="">
-            docs
-          </a>
-        </div> */}
       </div>
     </div>
   );
