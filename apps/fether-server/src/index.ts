cf2d8b59f223f6cd1f7a68eb90270213a9d7518f
0xFloop <floopcrypto@yahoo.com>
2023-03-14 15:36:38 -0400

    test

--- apps/fether-server/src/index.ts
+++ apps/fether-server/src/index.ts
@@ -16,7 +16,7 @@ app.post("/payload", jsonParser, (req, res) => {
   console.log(JSON.stringify(req.body, null, 2));
   for (let i = 0; i < req.body.commits.length; i++) {
     for (let j = 0; j < req.body.commits[i].modified.length; j++)
-      console.log("modified file : " + req.body.commits[i].modified[j]);
+      console.log("modified file: " + req.body.commits[i].modified[j]);
   }
   // res.send("POST REQ FROM GITHUB BELOW: \n" + JSON.stringify(req.body));
   //@ts-ignore
