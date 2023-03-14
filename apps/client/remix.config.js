5461ac9852bb19e8c4b03a0dd2e633110639d43e
0xFloop <floopcrypto@yahoo.com>
2023-03-13 20:14:13 -0400

    update config

--- apps/client/remix.config.js
+++ apps/client/remix.config.js
@@ -10,7 +10,6 @@ module.exports = {
   // so we default back to the standard build output.
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   serverBuildPath: "api/index.js",
-  devServerPort: 3003,
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
