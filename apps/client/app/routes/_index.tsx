d276edf2e5550cf122d191e9a9994cb45f1d930c
0xFloop <floopcrypto@yahoo.com>
2023-03-14 12:28:26 -0400

    mobile update

--- apps/client/app/routes/_index.tsx
+++ apps/client/app/routes/_index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
   return (
     <div className="w-screen h-screen overflow-hidden">
       <div className="m-auto relative flex flex-col justify-center items-center p-6 text-center w-full h-full">
-        <div className="font-display flex flex-row text-[150px] absolute bottom-12 md:bottom-4 left-6 align-bottom items-baseline">
+        <div className="font-display flex flex-row text-[150px] absolute bottom-16 md:bottom-4 left-6 align-bottom items-baseline">
           <h1 className="text-[100px] leading-[80px] md:text-[150px] md:leading-[120px]">fether</h1>
           <p className="text-base md:text-2xl">by floop</p>
         </div>
