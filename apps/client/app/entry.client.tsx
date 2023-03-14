4d11ca3ea9b6c3d30aa892f313cc8d30ce4d6519
0xFloop <floopcrypto@yahoo.com>
2023-03-13 22:52:02 -0400

    file add

---
+++ apps/client/app/entry.client.tsx
@@ -0,0 +1,21 @@
+import './polyfills';
+import { RemixBrowser } from '@remix-run/react';
+import { startTransition, StrictMode } from 'react';
+import { hydrateRoot } from 'react-dom/client';
+
+const hydrate = () => {
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <StrictMode>
+        <RemixBrowser />
+      </StrictMode>,
+    );
+  });
+};
+
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrate);
+} else {
+  window.setTimeout(hydrate, 1);
+}
