6b1934bccccd797a2004b50286c085951198ce53
0xFloop <floopcrypto@yahoo.com>
2023-03-14 12:45:11 -0400

    remove colors

--- apps/client/app/routes/_index.tsx
+++ apps/client/app/routes/_index.tsx
@@ -2,9 +2,9 @@ import Typewriter from "typewriter-effect";
 
 export default function Index() {
   return (
-    <div className="w-screen h-screen overflow-hidden bg-[#fffefa] text-[#211801]">
+    <div className="w-screen h-screen overflow-hidden">
       <div className="m-auto relative flex flex-col justify-center items-center p-6 text-center w-full h-full">
-        <div className="font-display flex flex-row text-[150px] absolute bottom-4 left-6 align-bottom items-baseline">
+        <div className="font-display flex flex-row absolute bottom-4 left-6 align-bottom items-baseline">
           <h1 className="text-[100px] leading-[80px] md:text-[150px] md:leading-[120px]">fether</h1>
           <p className="text-base md:text-2xl">by floop</p>
         </div>
