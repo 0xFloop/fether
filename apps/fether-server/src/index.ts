ba3131d663b1505c7b31a1c642cf53b02d02fc38
0xFloop <floopcrypto@yahoo.com>
2023-03-14 21:36:12 -0400

    grab only modified solidity file

--- apps/fether-server/src/index.ts
+++ apps/fether-server/src/index.ts
@@ -19,25 +19,27 @@ app.get("/", (req, res) => {
 });
 
 app.post("/payload", jsonParser, async (req, res) => {
-  console.log("POST REQ FROM GITHUB BELOW: \n" + JSON.stringify(req.body));
-
-  for (let i = 0; i < req.body.commits.length; i++) {
-    for (let j = 0; j < req.body.commits[i].modified.length; j++)
-      console.log("modified file: " + req.body.commits[i].modified[j]);
-  }
   //@ts-ignore
   const octokit = await octo.getInstallationOctokit(req.body.installation.id);
-  let contentsReq = await octokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
-    owner: "0xfloop",
-    repo: "fether",
-    path: "apps/fether-server/src/index.ts",
-    headers: {
-      "X-GitHub-Api-Version": "2022-11-28",
-      Accept: "application/vnd.github.raw",
-    },
-  });
 
-  console.log(contentsReq.data);
+  let modifiedContractPath: string;
+  for (let i = 0; i < req.body.commits.length; i++) {
+    for (let j = 0; j < req.body.commits[i].modified.length; j++)
+      if (req.body.commits[i].modified[j].slice(-3) == "sol") {
+        console.log("modified solidity file: " + req.body.commits[i].modified[j] + "\n\n\n");
+        modifiedContractPath = req.body.commits[i].modified[j];
+        let contentsReq = await octokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
+          owner: "0xfloop",
+          repo: "fether",
+          path: "apps/fether-server/src/index.ts",
+          headers: {
+            "X-GitHub-Api-Version": "2022-11-28",
+            Accept: "application/vnd.github.raw",
+          },
+        });
+        console.log(contentsReq.data);
+      }
+  }
 
   // res.send("POST REQ FROM GITHUB BELOW: \n" + JSON.stringify(req.body));
   //@ts-ignore
