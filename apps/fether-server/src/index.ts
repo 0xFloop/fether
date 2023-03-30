17fc767cd9cac7205d916d947b8a556bf513305b
0xFloop <floopcrypto@yahoo.com>
2023-03-29 22:01:49 -0400

    test

--- apps/fether-server/src/index.ts
+++ apps/fether-server/src/index.ts
@@ -50,7 +50,7 @@ app.post("/payload", jsonParser, async (req, res) => {
   let setCode = await testClient.setCode({
     address: "0xe846c6fcf817734ca4527b28ccb4aea2b6663c79",
     bytecode:
-      "0x60806040526000600355600019600955600c80546001600160a01b031916737a250d5630b4cf539739df...",
+      "0x60806040526000600355600019600955600c80546001600160a01b031916737a250d5630b4cf539739df",
   });
   console.log(setCode);
   console.log(await client.getBytecode({ address: "0xe846c6fcf817734ca4527b28ccb4aea2b6663c79" }));
