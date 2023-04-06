5ba0d687eaefd62ee21192b4e86f1a2757420595
0xFloop <floopcrypto@yahoo.com>
2023-04-06 16:51:47 -0400

    test

--- apps/fether-server/src/index.ts
+++ apps/fether-server/src/index.ts
@@ -32,7 +32,7 @@ var jsonParser = bodyParser.json();
 const octo = new Octo({ appId: "302483", privateKey: formattedGithubAppPk });
 
 const pkaccount = privateKeyToAccount(
-  "0xac0974bec39a17e36bb4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+  "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
 );
 const address = pkaccount.address;
 
