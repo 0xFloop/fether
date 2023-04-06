369cf7282d43bc3b6fecb8844dd56590c5b0b89d
0xFloop <floopcrypto@yahoo.com>
2023-04-06 16:46:13 -0400

    rebuild to test

--- apps/solidity/src/Counter.sol
+++ apps/solidity/src/Counter.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number = 37;
+    uint256 public number = 38;
     uint256 public number2 = 25;
     uint256 public number3 = 25;
 
