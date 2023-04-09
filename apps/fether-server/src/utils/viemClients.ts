3f1e55f4a0608bca7c35933a29ca44697e8217b9
0xFloop <floopcrypto@yahoo.com>
2023-04-09 14:04:29 -0400

    test

--- apps/fether-server/src/utils/viemClients.ts
+++ apps/fether-server/src/utils/viemClients.ts
@@ -1,11 +1,11 @@
 import { createPublicClient, createTestClient, createWalletClient, http } from "viem";
-import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
+import { privateKeyToAccount } from "viem/accounts";
 import { fetherChain } from "./config";
 import type {} from "abitype";
-const testprivateKey = generatePrivateKey();
 
-export const pkaccount = privateKeyToAccount(testprivateKey);
-
+export const pkaccount = privateKeyToAccount(
+  "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
+);
 export const address = pkaccount.address;
 
 export const walletClient = createWalletClient({
