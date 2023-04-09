3c9d857b48625d8f7ec4979503ba05411dc7b7fc
0xFloop <floopcrypto@yahoo.com>
2023-04-09 15:04:58 -0400

    new build

--- apps/solidity/src/Counter.sol
+++ apps/solidity/src/Counter.sol
@@ -2,20 +2,20 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number = 9110;
+    uint256 public number = 420;
     uint256 public number2 = 43;
     uint256 public number3 = 25;
 
-    function setNewNumber(uint256 newNumber) public {
+    function setTheNumber(uint256 newNumber) public {
         number = newNumber;
         number2 = number3 + newNumber;
     }
 
-    function getCurrentNumber() public view returns(uint256) {
+    function getTheNumber() public view returns(uint256) {
         return number;
     }
 
-    function incrementNumbers() public {
+    function incrementTheNumbers() public {
         number++;
         number2 = number3 + number2;
     }
--- apps/testing-client/app/routes/index.tsx
+++ apps/testing-client/app/routes/index.tsx
@@ -31,7 +31,7 @@ export default function Index() {
     const data = (await publicClient.readContract({
       address: fether.address,
       abi: fether.abi,
-      functionName: "getNumber",
+      functionName: "getCurrentNumber",
     })) as bigint;
 
     setNumber(Number(data));
