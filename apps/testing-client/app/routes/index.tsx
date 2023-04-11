21f38d270456d1b401f4bd4e508d60e9fb868d97
0xFloop <floopcrypto@yahoo.com>
2023-04-11 10:55:03 -0400

    add transaction submission test

--- apps/testing-client/app/routes/index.tsx
+++ apps/testing-client/app/routes/index.tsx
@@ -2,13 +2,21 @@ import { WagmiConfig, createClient, Chain } from "wagmi";
 import { Ethereum } from "@wagmi/core";
 import { ConnectKitProvider, ConnectKitButton, getDefaultClient } from "connectkit";
 import { useEffect, useState } from "react";
-import { createPublicClient, custom, http } from "viem";
+import {
+  createPublicClient,
+  createTestClient,
+  createWalletClient,
+  custom,
+  http,
+  parseEther,
+} from "viem";
 import Fether from "fetherkit";
 const fether = new Fether("abcd1234");
 
 export default function Index() {
   useEffect(() => {
     fether.init();
+    setWindowEthereum(window.ethereum as Ethereum);
 
     return () => {};
   }, []);
@@ -21,6 +29,7 @@ export default function Index() {
     })
   );
   const [number, setNumber] = useState<number>();
+  const [windowEthereum, setWindowEthereum] = useState<Ethereum>();
 
   let publicClient = createPublicClient({
     chain: fether.chain,
@@ -34,7 +43,34 @@ export default function Index() {
       functionName: "getLeNumber",
     });
 
-    setNumber(Number(test));
+    setNumber(Number(number));
+  };
+
+  const sendContractTransaction = async (newNum: number) => {
+    let adminClient = createTestClient({
+      chain: fether.chain,
+      mode: "anvil",
+      transport: http(),
+    });
+    await adminClient.setBalance({
+      address: "0xDA1B24C1eb29DC9Fe733EE93a5d4ff727Cc4ea7f",
+      value: parseEther("1"),
+    });
+    let walletClient = createWalletClient({
+      chain: fether.chain,
+      transport: custom(windowEthereum as Ethereum),
+    });
+
+    const [address] = await walletClient.getAddresses();
+
+    const { request } = await publicClient.simulateContract({
+      account: address,
+      address: fether.address,
+      abi: fether.abi,
+      functionName: "setLeNumber",
+      args: [70],
+    });
+    await walletClient.writeContract(request);
   };
 
   return (
@@ -52,6 +88,9 @@ export default function Index() {
             <li>{JSON.stringify(method["name"])}</li>
           ))}{" "}
         </ul>
+        <button onClick={async () => await sendContractTransaction(70)}>
+          click to get update number
+        </button>
       </ConnectKitProvider>
     </WagmiConfig>
   );
