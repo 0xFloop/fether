1653f5433d3a49bdee7822297ac8b8646d23b583
0xFloop <floopcrypto@yahoo.com>
2023-04-19 10:24:49 -0400

    navbar

--- apps/client/app/routes/alpha.dashboard.tsx
+++ apps/client/app/routes/alpha.dashboard.tsx
@@ -15,10 +15,35 @@ export const loader = async ({ request }: LoaderArgs) => {
 
   let userData = await db.user.findUnique({ where: { id: user.get("userId") } });
 
+  console.log(userData);
+
   return userData;
 };
 
 export default function Index() {
   const data = useLoaderData();
-  return <div className="w-screen h-screen overflow-hidden">{data.username}</div>;
+
+  return (
+    <div className="w-screen h-screen overflow-hidden display flex flex-col">
+      <div
+        id="navbar"
+        className="h-20 border-b border-b-black flex flex-row justify-between px-8 items-center"
+      >
+        <h2 id="logo" className="text-5xl flex-1">
+          fether
+        </h2>
+        <div id="nav-links" className="flex flex-row gap-32 flex-1">
+          <a href="/alpha/dashboard">dashboard</a>
+          <a href="/alpha/documentation">documentation</a>
+          <a href="/alpha/contact">contact</a>
+        </div>
+        <div className="flex-1">
+          <button id="signout" className="float-right">
+            signout
+          </button>
+        </div>
+      </div>
+      <div id="content"></div>
+    </div>
+  );
 }
