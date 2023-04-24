6317fa63041373f1f935a5f56a34dbde47dea348
0xFloop <floopcrypto@yahoo.com>
2023-04-24 12:00:32 -0400

    move to log in with github; remove signup page

--- apps/client/app/routes/alpha.login.tsx
+++ apps/client/app/routes/alpha.login.tsx
@@ -12,60 +12,51 @@ import { validateCredentials } from "~/utils/validateUser.server";
 export const loader = async ({ request }: LoaderArgs) => {
   const session = await getSession(request.headers.get("Cookie"));
   const user = await userGetSession(request.headers.get("Cookie"));
+  if (!session.has("alphaKey")) throw redirect("/");
   if (user.has("userId")) throw redirect("/alpha/dashboard");
   else return null;
 };
 
-export async function action({ request }: ActionArgs) {
-  const session = await userGetSession(request.headers.get("Cookie"));
-  const form = await request.formData();
-  const username = form.get("username") as string;
-  const password = form.get("password") as string;
-  let userData;
-  try {
-    userData = await validateCredentials({ username, password });
-  } catch (err: unknown) {
-    if (err instanceof Error) {
-      return json({ message: err.message });
-    } else return json({ message: "Unknown error" });
-  }
+// export async function action({ request }: ActionArgs) {
+//   const session = await userGetSession(request.headers.get("Cookie"));
+//   const form = await request.formData();
+//   const username = form.get("username") as string;
+//   const password = form.get("password") as string;
+//   let userData;
+//   try {
+//     userData = await validateCredentials({ username, password });
+//   } catch (err: unknown) {
+//     if (err instanceof Error) {
+//       return json({ message: err.message });
+//     } else return json({ message: "Unknown error" });
+//   }
 
-  session.set("userId", userData.id as string);
+//   session.set("userId", userData.id as string);
 
-  // Login succeeded, send them to the home page.
-  return redirect("/alpha/dashboard", {
-    headers: {
-      "Set-Cookie": await userCommitSession(session),
-    },
-  });
-}
+//   // Login succeeded, send them to the home page.
+//   return redirect("/alpha/dashboard", {
+//     headers: {
+//       "Set-Cookie": await userCommitSession(session),
+//     },
+//   });
+// }
 
 export default function Index() {
-  const data = useActionData<typeof action>();
+  // const data = useActionData<typeof action>();
+  const clientId = "1755f9594459f4e4030c";
+  const redirectUri = "https://442a94caf4b9.ngrok.app/gh-callback";
+  function handleLogin() {
+    const scope = "user:email";
+    const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`;
+    return url;
+  }
 
   return (
     <div className="w-screen h-screen overflow-hidden">
       {" "}
       <div className="relative left-0 w-full flex flex-col h-full items-center align-middle justify-center">
         <div className="h-auto w-auto relative border-2 border-black rounded p-10">
-          <p className="font-sans text-2xl inline-block">Log in</p>
-          <Form method="post" className="mt-5 flex flex-col">
-            <input
-              type="text"
-              placeholder="username"
-              name="username"
-              className="border-black rounded"
-            />
-            <input
-              type="password"
-              placeholder="password"
-              name="password"
-              className="border-black rounded mt-5"
-            />
-            <button type="submit" className="font-sans text-base mt-5 inline-block">
-              Submit
-            </button>
-          </Form>
+          <Link to={handleLogin()}>Log in with GitHub</Link>
 
           {data && (
             <p className="font-sans text-red-500 text-base mt-4 inline-block">{data.message}</p>
