1a159ba44ba7374520feb7e476d73ada9a163037
0xFloop <floopcrypto@yahoo.com>
2023-04-27 11:29:50 -0400

    add repoid check on payload endpoint

--- apps/fether-server/src/index.ts
+++ apps/fether-server/src/index.ts
@@ -68,7 +68,12 @@ app.post("/payload", jsonParser, async (req, res) => {
     console.log(req.body);
 
     //needs to check that  the push is to the same repository name as the one associated with the user
-    if (associatedUserData && associatedUserData.ApiKey && associatedUserData.Repository) {
+    if (
+      associatedUserData &&
+      associatedUserData.ApiKey &&
+      associatedUserData.Repository &&
+      associatedUserData.Repository.id == req.body.repository.id
+    ) {
       for (let i = 0; i < req.body.commits.length; i++) {
         for (let j = 0; j < req.body.commits[i].modified.length; j++)
           if (req.body.commits[i].modified[j].slice(-3) == "sol") {
