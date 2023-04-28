cd87f294ebb6ee5e8526adf31b0eb26ab364f070
0xFloop <floopcrypto@yahoo.com>
2023-04-28 09:14:02 -0400

    test

--- apps/solidity/src/Counter.sol
+++ apps/solidity/src/Counter.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number = 67;
-    uint256 public number2 = 49;
+    uint256 public number2 = 50;
     uint256 public number3 = 25;
 
     function setLeNumber(uint256 newNumber) public {
@@ -11,7 +11,7 @@ contract Counter {
         number2 = number3 + newNumber;
     }
 
-    function getLeNumber() public view returns(uint256) {
+    function getLeNumber() public view returns (uint256) {
         return number;
     }
 
