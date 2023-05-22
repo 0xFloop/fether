0da0e2783f5d05c257b76840242b256826c463b2
0xFloop <floopcrypto@yahoo.com>
2023-05-22 17:34:03 -0400

    add new contract for testing multiple contracts

---
+++ apps/solidity/src/TestContract.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract TestContract {
+    uint256 public number = 82;
+    uint256 public number2 = 73;
+    uint256 public number3 = 25;
+
+    function setOwensNumber(uint256 newNumber) public {
+        number = newNumber;
+        number2 = number3 + newNumber;
+    }
+
+    function getOwensNumber() public view returns (uint256) {
+        return number;
+    }
+
+    function incrementLeNumbers() public {
+        number++;
+        number2 = number3 + number2;
+    }
+}
