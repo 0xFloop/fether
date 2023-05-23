d2f50782abb11281267589dfefaa03ce263d522a
0xFloop <floopcrypto@yahoo.com>
2023-05-23 09:25:02 -0400

    add method to get all the sol files from a repo

--- apps/client/app/utils/octo.server.ts
+++ apps/client/app/utils/octo.server.ts
@@ -1,4 +1,6 @@
 import { App as Octo } from "octokit";
+import { db } from "~/db.server";
+import { z } from "zod";
 
 function getGithubPk() {
   const githubAppPk = process.env.appPK as string;
@@ -18,3 +20,45 @@ export const getUserRepositories = async (githubInstallationId: string) => {
   });
   return repositories;
 };
+
+export const getSolFileNames = async (githubInstallationId: string): Promise<String[]> => {
+  const octokit = await octo.getInstallationOctokit(parseInt(githubInstallationId));
+
+  let repoData = await db.user.findUnique({
+    where: { githubInstallationId },
+    include: { Repository: true },
+  });
+
+  let ownerName = repoData?.Repository?.name.split("/")[0];
+  let repoName = repoData?.Repository?.name.split("/")[1];
+
+  let fileNames: String[] = [];
+
+  if (!ownerName || !repoName) throw new Error("No owner or repo name found");
+
+  let contractSrcFolder = await octokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
+    owner: ownerName,
+    repo: repoName,
+    path: "/apps/solidity/src",
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+      Accept: "application/vnd.github.raw",
+    },
+  });
+
+  let zodFileArray = zodArrayOfGithubFiles.safeParse(contractSrcFolder.data);
+
+  if (!zodFileArray.success) throw new Error("No solidity src folder found");
+
+  for (let i = 0; i < zodFileArray.data.length; i++) {
+    fileNames.push(zodFileArray.data[i].name);
+  }
+
+  return fileNames;
+};
+
+export const zodArrayOfGithubFiles = z.array(
+  z.object({
+    name: z.string(),
+  })
+);
