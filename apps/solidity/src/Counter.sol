a203b7204341b3652175e2b6a97c81a78bf2dfca
0xFloop <floopcrypto@yahoo.com>
2023-05-26 16:53:40 -0400

    add functions for dashboard testing

--- apps/solidity/src/Counter.sol
+++ apps/solidity/src/Counter.sol
@@ -5,14 +5,27 @@ contract Counter {
     uint256 public number = 84;
     uint256 public number2 = 73;
     uint256 public number3 = 25;
+    string public letter = "A";
 
     function setOwensNumber(uint256 newNumber) public {
         number = newNumber;
         number2 = number3 + newNumber;
     }
 
+    function setNumberAndLetter(
+        uint256 newNumber,
+        string memory newString
+    ) public {
+        number = newNumber;
+        letter = newString;
+    }
+
     function getOwensNumber() public view returns (uint256) {
         return number;
+    }
+
+    function getNumberAndLetter() public view returns (string memory, uint256) {
+        return (letter, number);
     }
 
     function incrementLeNumbers() public {
