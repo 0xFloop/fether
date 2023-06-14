109d8e4988b0301ba8daea28a5eac2957116d36e
0xFloop <floopcrypto@yahoo.com>
2023-06-14 16:45:31 -0400

    move wagmi/rainbow setup to root

--- apps/client/app/root.tsx
+++ apps/client/app/root.tsx
@@ -1,5 +1,9 @@
+import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import { Chain, WagmiConfig, configureChains, createConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 
 import stylesheet from "~/tailwind.css";
 
@@ -14,6 +18,39 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
+const BaseFetherChain: Chain = {
+  id: 696969,
+  name: "Fether",
+  network: "fether",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Fether",
+    symbol: "FEth",
+  },
+  rpcUrls: {
+    default: {
+      http: [`https://fether-testing.ngrok.app/rpc/GlobalLoader`],
+    },
+    public: { http: [`https://fether-testing.ngrok.app/rpc/GlobalLoader`] },
+  },
+  testnet: false,
+};
+const { chains, publicClient } = configureChains(
+  [BaseFetherChain],
+  [alchemyProvider({ apiKey: "NOTNEEDED" }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Fether",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
 export default function App() {
   return (
     <html lang="en" className="h-full">
@@ -23,7 +60,11 @@ export default function App() {
       </head>
       <body>
         <div className="mx-auto min-h-screen">
-          <Outlet />
+          <WagmiConfig config={wagmiConfig}>
+            <RainbowKitProvider chains={chains}>
+              <Outlet />
+            </RainbowKitProvider>
+          </WagmiConfig>
         </div>
         <ScrollRestoration />
         <Scripts />
