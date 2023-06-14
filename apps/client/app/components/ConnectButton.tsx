73f3fc65624cc6f25ed01abf0f3d31454eaf16e3
0xFloop <floopcrypto@yahoo.com>
2023-06-14 16:45:56 -0400

    custom rainbowkit connect button

---
+++ apps/client/app/components/ConnectButton.tsx
@@ -0,0 +1,74 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+export const CustomConnectButton = () => {
+  return (
+    <ConnectButton.Custom>
+      {({
+        account,
+        chain,
+        openAccountModal,
+        openChainModal,
+        openConnectModal,
+        authenticationStatus,
+        mounted,
+      }) => {
+        // Note: If your app doesn't use authentication, you
+        // can remove all 'authenticationStatus' checks
+        const ready = mounted && authenticationStatus !== "loading";
+        const connected =
+          ready &&
+          account &&
+          chain &&
+          (!authenticationStatus || authenticationStatus === "authenticated");
+        return (
+          <div
+            {...(!ready && {
+              "aria-hidden": true,
+              style: {
+                opacity: 0,
+                pointerEvents: "none",
+                userSelect: "none",
+              },
+            })}
+          >
+            {(() => {
+              if (!connected) {
+                return (
+                  <button
+                    data-connected={Boolean(account)}
+                    className="text-white bg-black py-2 px-4 border rounded-lg"
+                    onClick={openConnectModal}
+                    type="button"
+                  >
+                    Connect
+                  </button>
+                );
+              }
+              if (chain.unsupported) {
+                return (
+                  <button
+                    data-connected={Boolean(account)}
+                    className="text-white bg-black py-2 px-4 border rounded-lg"
+                    onClick={openChainModal}
+                    type="button"
+                  >
+                    Wrong network
+                  </button>
+                );
+              }
+              return (
+                <button
+                  data-connected={Boolean(account)}
+                  className="text-white bg-black py-2 px-4 border rounded-lg"
+                  onClick={openAccountModal}
+                  type="button"
+                >
+                  {account.displayName}
+                </button>
+              );
+            })()}
+          </div>
+        );
+      }}
+    </ConnectButton.Custom>
+  );
+};
