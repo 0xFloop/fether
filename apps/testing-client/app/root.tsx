0a7f0681b91be5a959b217e8de24c3b4e2a77fd1
0xFloop <floopcrypto@yahoo.com>
2023-06-19 17:31:09 -0400

    return apikey from loader on test client

--- apps/testing-client/app/root.tsx
+++ apps/testing-client/app/root.tsx
@@ -1,5 +1,13 @@
-import type { MetaFunction } from "@remix-run/node";
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import type { LoaderArgs, MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+} from "@remix-run/react";
 import { ConnectKitProvider, getDefaultClient } from "connectkit";
 import { FetherProvider, BaseFetherChain } from "fetherkit";
 import { WagmiConfig, createClient } from "wagmi";
@@ -9,7 +17,16 @@ export const meta: MetaFunction = () => ({
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+export const loader = async ({ request }: LoaderArgs) => {
+  //validate session cookie
+
+  const apiKey = process.env.API_KEY as string;
+
+  return apiKey;
+};
 export default function App() {
+  const apiKey = useLoaderData<typeof loader>();
+
   const client = createClient(
     getDefaultClient({
       appName: "Fether Testing Client",
@@ -17,6 +34,7 @@ export default function App() {
       chains: [BaseFetherChain],
     })
   );
+
   return (
     <html lang="en">
       <head>
@@ -24,7 +42,7 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <FetherProvider apiKey={process.env.API_KEY as string}>
+        <FetherProvider apiKey={apiKey}>
           <WagmiConfig client={client}>
             <ConnectKitProvider>
               <Outlet />
