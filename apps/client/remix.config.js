9714454b4266b47e1231acd6eaf71e887806e192
0xFloop <floopcrypto@yahoo.com>
2023-06-22 16:51:52 -0400

    revert some stuff

--- apps/client/remix.config.js
+++ apps/client/remix.config.js
@@ -1,22 +1,28 @@
 /** @type {import('@remix-run/dev').AppConfig} */
+
+const isDevelopment = process.env.NODE_ENV === "development";
+
 module.exports = {
-  future: {
-    v2_routeConvention: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_errorBoundary: true,
-  },
-  cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*"],
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  cacheDirectory: "./node_modules/.cache/remix",
+  server: isDevelopment ? undefined : "./server.ts",
   serverBuildPath: "api/index.js",
   serverDependenciesToBundle: [
     "database",
-    /^@?connectkit.*/,
-    /^@?wagmi.*/,
     "@rainbow-me/rainbowkit",
+    "@rainbow-me/rainbowkit/wallets",
+    /^@?wagmi.*/,
+    /^@?connectkit.*/,
     /^@?rainbow-me.*/,
   ],
+  watchPaths: async () => {
+    return ["../../packages/database/prisma/schema.prisma"];
+  },
   tailwind: true,
+  future: {
+    v2_errorBoundary: true,
+    v2_routeConvention: true,
+    v2_meta: true,
+    v2_dev: true,
+  },
 };
