a6c6234b0ee79a8c1e37b5d29c88bca34adcbe15
0xFloop <floopcrypto@yahoo.com>
2023-06-22 10:13:42 -0400

    add publicclient type to fix exporting error

--- apps/fether-server/src/utils/viemClients.ts
+++ apps/fether-server/src/utils/viemClients.ts
@@ -1,7 +1,6 @@
-import { createPublicClient, createTestClient, createWalletClient, http } from "viem";
+import { PublicClient, createPublicClient, createTestClient, createWalletClient, http } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { fetherChain } from "./config";
-import type {} from "abitype";
 
 const pkaccount = privateKeyToAccount(
   "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
@@ -14,7 +13,7 @@ export const walletClient = createWalletClient({
   transport: http(),
 });
 
-export const publicClient = createPublicClient({
+export const publicClient: PublicClient = createPublicClient({
   chain: fetherChain,
   transport: http(),
 });
