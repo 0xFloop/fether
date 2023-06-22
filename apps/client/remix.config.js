240049fca61d6973fe0544030cea0c6ececb1b19
0xFloop <floopcrypto@yahoo.com>
2023-06-22 14:56:09 -0400

    config update

--- apps/client/remix.config.js
+++ apps/client/remix.config.js
@@ -1,30 +1,26 @@
 /** @type {import('@remix-run/dev').AppConfig} */
+
 const isDevelopment = process.env.NODE_ENV === "development";
 
 module.exports = {
-  future: {
-    v2_routeConvention: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_errorBoundary: true,
-  },
-  cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*"],
+  cacheDirectory: "./node_modules/.cache/remix",
   server: isDevelopment ? undefined : "./server.ts",
   serverBuildPath: "api/index.js",
+  serverDependenciesToBundle: [
+    "database",
+    "@rainbow-me/rainbowkit",
+    "@rainbow-me/rainbowkit/wallets",
+    /^@?wagmi.*/,
+  ],
   watchPaths: async () => {
     return ["../../packages/database/prisma/schema.prisma"];
   },
-  serverDependenciesToBundle: [
-    "database",
-    /^@?connectkit.*/,
-    /^@?wagmi.*/,
-    "@rainbow-me/rainbowkit",
-    /^@?rainbow-me.*/,
-    "@rainbow-me/rainbowkit/wallets",
-    /^@?wagmi.*/,
-    /.*/,
-  ],
   tailwind: true,
+  future: {
+    v2_errorBoundary: true,
+    v2_routeConvention: true,
+    v2_meta: true,
+    v2_dev: true,
+  },
 };
