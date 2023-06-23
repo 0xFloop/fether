01677455348bde867fb912cda26b132609c3161c
0xFloop <floopcrypto@yahoo.com>
2023-06-23 12:13:25 -0400

    update db.server file

--- apps/client/app/utils/db.server.ts
+++ apps/client/app/utils/db.server.ts
@@ -1,8 +1,21 @@
-import { PrismaClient } from "database";
-import { singleton } from "./singleton.server";
+import { PrismaClient } from "@prisma/client";
 
-// Creates a singleton instance of PrismaClient
-const db = singleton("db", () => new PrismaClient({ log: ["error", "warn"] }));
-db.$connect();
+let db: PrismaClient;
+
+declare global {
+  var __db: PrismaClient | undefined;
+}
+
+// this is needed because in development we don't want to restart
+// the server with every change, but we want to make sure we don't
+// create a new connection to the DB with every change either.
+if (process.env.NODE_ENV === "production") {
+  db = new PrismaClient();
+} else {
+  if (!global.__db) {
+    global.__db = new PrismaClient();
+  }
+  db = global.__db;
+}
 
 export { db };
