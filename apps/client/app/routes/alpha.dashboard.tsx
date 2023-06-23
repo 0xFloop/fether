133b407c1b967bb102eb847ec66e322cf4e5aa1b
0xFloop <floopcrypto@yahoo.com>
2023-06-23 14:53:34 -0400

    test logging

--- apps/client/app/routes/alpha.dashboard.tsx
+++ apps/client/app/routes/alpha.dashboard.tsx
@@ -148,6 +148,8 @@ export const action = async ({ request }: ActionArgs) => {
         };
 
       case "deployContract":
+        console.log(process.env.NODE_ENV);
+        console.log("deployContract");
         await deployContract(githubInstallationId as string, associatedUser);
 
         return {
