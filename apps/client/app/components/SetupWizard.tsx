21bfdc904ab11f0742ef501f01b4d6517de97191
0xFloop <floopcrypto@yahoo.com>
2023-07-17 12:55:01 -0700

    dynamic setup step selection

--- apps/client/app/components/SetupWizard.tsx
+++ apps/client/app/components/SetupWizard.tsx
@@ -2,6 +2,7 @@ import { Form, useActionData, useLoaderData, useNavigation } from "@remix-run/re
 import { Loader } from "lucide-react";
 import { useState } from "react";
 import { action, loader } from "~/routes/alpha.dashboard";
+import { setupSteps } from "./setupSteps";
 
 interface SetupWizardProps {
   userData: ReturnType<typeof useLoaderData<typeof loader>>;
@@ -11,14 +12,37 @@ interface SetupWizardProps {
 }
 
 const SetupWizard: React.FC<SetupWizardProps> = (props: SetupWizardProps) => {
+  const [setupStep, setSetupStep] = useState(props.setupStep);
   return (
     <div className="w-[1100px] max-w-7xl h-[750px] flex flex-row justify-between items-center">
       <div className="text-white rounded-l-3xl h-full w-1/3 p-4 bg-[#1E1E1E] flex flex-col gap-4">
         <p className="text-lg mt-4 bg-secondary-blue w-20 rounded-lg text-center">Alpha</p>
         <h1 className="text-5xl">Welcome to Fether setup wizard</h1>
         <p className="text-lg">Let's get things set up for you!</p>
-        <div id="step-selector" className="flex-1 border border-white ">
-          <p>hello</p>
+        <div id="step-selector" className="flex flex-col justify-between flex-1">
+          {setupSteps.map((step, index) => (
+            <button
+              id={step.stepNumber}
+              onClick={() => setSetupStep(index + 1)}
+              className={
+                (setupStep == index + 1 ? "bg-secondary-blue " : "") +
+                "w-full p-4 flex flex-row items-center border rounded-lg border-[#6161FF]"
+              }
+            >
+              <div className="ml-2 text-base">
+                {setupStep > index + 1 ? (
+                  <img
+                    src="/images/setupCheck.svg"
+                    alt="step complete checkmark"
+                    className="w-6 h-8"
+                  ></img>
+                ) : (
+                  <p className="text-2xl">{step.stepNumber}</p>
+                )}
+              </div>
+              <p className="ml-2 text-base">{step.name}</p>
+            </button>
+          ))}
         </div>
       </div>
       <div className="text-white rounded-r-3xl h-full bg-[#545454] flex-1 ">right</div>
