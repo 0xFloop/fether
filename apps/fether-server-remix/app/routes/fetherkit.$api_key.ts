233affa5e27841d087e5b4d4db861e8652899fa1
0xFloop <floopcrypto@yahoo.com>
2023-08-14 14:32:07 -0400

    fix fetherkit api error throw

--- apps/fether-server-remix/app/routes/fetherkit.$api_key.ts
+++ apps/fether-server-remix/app/routes/fetherkit.$api_key.ts
@@ -8,21 +8,8 @@ export const loader = async ({ request, params }: LoaderArgs) => {
 
     let validated = await validateSender(api_key);
     if (!validated.success) {
-      throw new Response(
-        JSON.stringify("FetherKit: Invalid Fether api key. Sign up here https://www.fether.xyz."),
-        {
-          status: 469,
-          statusText: JSON.stringify(
-            "FetherKit: Invalid Fether api key. Sign up here https://www.fether.xyz."
-          ),
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json",
-            Allow: "GET",
-            "Access-Control-Allow-Headers": "Origin, Content-Type",
-            "Access-Control-Allow-Methods": "GET",
-          },
-        }
+      throw new Error(
+        JSON.stringify("FetherKit: Invalid Fether api key. Sign up here https://www.fether.xyz.")
       );
     } else {
       return new Response(JSON.stringify(validated.apiKeyData?.associatedUser.Repository), {
@@ -37,16 +24,6 @@ export const loader = async ({ request, params }: LoaderArgs) => {
       });
     }
   } catch (err) {
-    throw new Response(JSON.stringify(err), {
-      status: 469,
-      statusText: JSON.stringify(err),
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json",
-        Allow: "GET",
-        "Access-Control-Allow-Headers": "Origin, Content-Type",
-        "Access-Control-Allow-Methods": "GET",
-      },
-    });
+    throw new Error(JSON.stringify(err));
   }
 };
