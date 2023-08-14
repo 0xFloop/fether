cc2daca9e0aef5c9de21c5295f158e7dffba702e
0xFloop <floopcrypto@yahoo.com>
2023-08-14 14:22:59 -0400

    update fetherkit endpoint

--- apps/fether-server-remix/app/routes/fetherkit.$api_key.ts
+++ apps/fether-server-remix/app/routes/fetherkit.$api_key.ts
@@ -8,32 +8,45 @@ export const loader = async ({ request, params }: LoaderArgs) => {
 
     let validated = await validateSender(api_key);
     if (!validated.success) {
-      let error = {
-        jsonrpc: "2.0",
-        error: {
-          code: -32000,
-          message: "FetherKit: Invalid Fether api key. Sign up here https://www.fether.xyz.",
-        },
-        id: "1",
-      };
-
-      throw new Response(JSON.stringify(error), {
-        status: 500,
-        statusText: JSON.stringify(error),
-        headers: { "Access-Control-Allow-Origin": "*" },
-      });
+      throw new Response(
+        JSON.stringify("FetherKit: Invalid Fether api key. Sign up here https://www.fether.xyz."),
+        {
+          status: 469,
+          statusText: JSON.stringify(
+            "FetherKit: Invalid Fether api key. Sign up here https://www.fether.xyz."
+          ),
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+            Allow: "GET",
+            "Access-Control-Allow-Headers": "Origin, Content-Type",
+            "Access-Control-Allow-Methods": "GET",
+          },
+        }
+      );
     } else {
       return new Response(JSON.stringify(validated.apiKeyData?.associatedUser.Repository), {
         status: 200,
-        headers: { "Access-Control-Allow-Origin": "*" },
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+          Allow: "GET",
+          "Access-Control-Allow-Headers": "Origin, Content-Type",
+          "Access-Control-Allow-Methods": "GET",
+        },
       });
     }
   } catch (err) {
-    console.log("ERROR OCCURED: \n", err);
     throw new Response(JSON.stringify(err), {
-      status: 500,
+      status: 469,
       statusText: JSON.stringify(err),
-      headers: { "Access-Control-Allow-Origin": "*" },
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+        Allow: "GET",
+        "Access-Control-Allow-Headers": "Origin, Content-Type",
+        "Access-Control-Allow-Methods": "GET",
+      },
     });
   }
 };
