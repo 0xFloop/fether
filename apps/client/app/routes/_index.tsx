659afe1b02683e6784ba2542fbc681e899dffe5a
0xFloop <floopcrypto@yahoo.com>
2023-08-17 14:03:06 -0400

    remove 'by floop'

--- apps/client/app/routes/_index.tsx
+++ apps/client/app/routes/_index.tsx
@@ -64,7 +64,6 @@ export default function Index() {
       <div className="m-auto relative flex flex-col justify-center items-center p-6 text-center w-full h-full">
         <div className="font-display flex flex-row absolute bottom-4 left-6 align-bottom items-baseline">
           <h1 className="text-[100px] leading-[80px] md:text-[150px] md:leading-[120px]">fether</h1>
-          <p className="text-base md:text-2xl">by floop</p>
         </div>
         <div className="font-display flex flex-row absolute bottom-4 right-6 align-bottom items-baseline">
           {hasAccess && <Link to="/alpha">Access Alpha</Link>}
