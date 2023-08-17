894b7441e525e92d3f732a4a4e0e273af978a6e8
0xFloop <floopcrypto@yahoo.com>
2023-08-17 12:32:12 -0400

    move types to single types file

--- apps/client/app/types.d.ts
+++ apps/client/app/types.d.ts
@@ -11,4 +11,35 @@ export type UserWithKeyRepoActivity =
     })
   | null;
 
-type RepoData = { repoName: string; repoId: string };
+export type RepoData = { repoName: string; repoId: string };
+
+export type ContractReturnItem = {
+  name: string;
+  value: any;
+};
+export type ContractReturn = { methodName: string; returnItems: ContractReturnItem[] };
+
+export type TxDetails = {
+  hash: string;
+  status: "Succeded" | "Failed" | "Pending" | "Unknown";
+  timestamp: number;
+  blockNumber: number;
+  from: string;
+  to: string | null;
+  created: string | null;
+  gasUsed: string;
+  maxPriorityFee: string;
+  maxFee: string;
+  nonce: number;
+  transactionFee: string;
+};
+
+export type DashboardActionReturn = {
+  originCallForm: string | null;
+  chosenRepoName: string | null;
+  repositories: RepoData[] | null;
+  solFilesFromChosenRepo: string[] | null;
+  chosenFileName: string | null;
+  txDetails: TxDetails | null;
+  error: any;
+};
