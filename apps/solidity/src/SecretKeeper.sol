109e0cc1c2e7bd7bdb02d66694127a671599c081
0xFloop <floopcrypto@yahoo.com>
2023-09-04 19:33:09 -0400

    update contract to test struct params

--- apps/solidity/src/SecretKeeper.sol
+++ apps/solidity/src/SecretKeeper.sol
@@ -5,6 +5,13 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract SecretKeeper is Ownable {
     string public secretPhrase = "fixing shit";
     uint256 public secretNumber = 100;
+    SecretStorage public secretInStorage =
+        SecretStorage(420, "this is a secret");
+
+    struct SecretStorage {
+        uint secretNumber;
+        string secretPrase;
+    }
 
     function updateSecretPhrase(string memory _secretPhrase) public {
         secretPhrase = _secretPhrase;
@@ -16,6 +23,10 @@ contract SecretKeeper is Ownable {
         returns (string memory _secretPhrase)
     {
         _secretPhrase = secretPhrase;
+    }
+
+    function updateSecretInStorage(SecretStorage memory secretParam) public {
+        secretInStorage = secretParam;
     }
 
     function updateSecretNumber(uint256 _secretNumber) public {
