ecd4b75f3e05d3a7605eacfd0d5765fdf5e088d1
0xFloop <floopcrypto@yahoo.com>
2023-10-24 15:09:45 -0400

    move error to alpha.tsx to use loaderData

--- apps/client/app/routes/alpha.tsx
+++ apps/client/app/routes/alpha.tsx
@@ -1,5 +1,15 @@
 import { ActionArgs, LoaderArgs, json, redirect } from "@vercel/remix";
-import { Form, Link, Outlet, useActionData, useLoaderData } from "@remix-run/react";
+import {
+  Form,
+  Link,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  useActionData,
+  useLoaderData,
+  useRouteError,
+} from "@remix-run/react";
 import { db } from "../utils/db.server";
 import { getSession, commitSession } from "../utils/alphaAccessKeySession.server";
 import {
@@ -9,6 +19,7 @@ import {
 import { WalletProvider } from "~/components/WalletProvider";
 import { createContext, useState, Dispatch, SetStateAction } from "react";
 import { Navbar } from "~/components/Navbar";
+import { Footer } from "~/components/Footer";
 
 export const loader = async ({ request, params }: LoaderArgs) => {
   const session = await getSession(request.headers.get("Cookie"));
@@ -40,7 +51,66 @@ export default function Index() {
       <WalletProvider>
         <Navbar hasAccess={userHasId} displayInvites={true} />
         <Outlet />
+        <Footer />
       </WalletProvider>
     </DisplayCodesContext.Provider>
   );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  const userHasId = useLoaderData<typeof loader>();
+
+  if (error instanceof Error) {
+    return (
+      <html>
+        <head>
+          <title>Oh no!</title>
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          <Navbar hasAccess={userHasId} displayInvites={false} />
+          <div className="h-screen w-screen flex flex-col justify-center align-middle items-center bg-primary-gray text-white font-primary">
+            <h1>Uh oh ...</h1>
+            <p>Something went wrong.</p>
+            <pre>Error: {error.message}</pre>
+            <Link
+              to="/alpha/dashboard"
+              className="mt-4 border border-off-white/25 text-off-white/25 px-4"
+            >
+              Back to dashboard
+            </Link>
+          </div>
+          <Scripts />
+        </body>
+      </html>
+    );
+  } else {
+    return (
+      <html>
+        <head>
+          <title>Oh no!</title>
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          <Navbar hasAccess={userHasId} displayInvites={false} />
+          <div className="h-screen w-screen flex flex-col justify-center align-middle items-center bg-primary-gray text-white font-primary">
+            <h1>Uh oh ...</h1>
+            <p>Something went wrong.</p>
+            <pre>Error: {JSON.stringify(error)}</pre>
+            <Link
+              to="/alpha/dashboard"
+              className="mt-4 border border-off-white/25 text-off-white/25 px-4"
+            >
+              Back to dashboard
+            </Link>
+          </div>
+
+          <Scripts />
+        </body>
+      </html>
+    );
+  }
 }
