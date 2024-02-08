7def86fec8cffbc3d16de9cf8afbe84598e78c20
0xFloop <floopcrypto@yahoo.com>
2024-02-08 10:44:08 -0700

    fix git cache

---
+++ apps/fether-server-rs/target/debug/deps/fether_server_rs-db21bcce64e8682d.d
@@ -0,0 +1,5 @@
+/home/unnamed/Desktop/code/fether/apps/fether-server-rs/target/debug/deps/libfether_server_rs-db21bcce64e8682d.rmeta: src/main.rs
+
+/home/unnamed/Desktop/code/fether/apps/fether-server-rs/target/debug/deps/fether_server_rs-db21bcce64e8682d.d: src/main.rs
+
+src/main.rs:
---
+++ apps/fether-server-rs/target/debug/deps/fether_server_rs-df1abb05a1b16fb4.d
@@ -0,0 +1,5 @@
+/home/unnamed/Desktop/code/fether/apps/fether-server-rs/target/debug/deps/libfether_server_rs-df1abb05a1b16fb4.rmeta: src/main.rs
+
+/home/unnamed/Desktop/code/fether/apps/fether-server-rs/target/debug/deps/fether_server_rs-df1abb05a1b16fb4.d: src/main.rs
+
+src/main.rs:
