a1efd27f48aa884efea45d136e1ff3b3af3fff78
0xFloop <95703085+0xFloop@users.noreply.github.com>
2024-04-02 18:08:56 -0600

    fug

--- apps/fether-server-rs/src/main.rs
+++ apps/fether-server-rs/src/main.rs
@@ -323,7 +323,7 @@ async fn github_payload_handler(
 
     println!("heer2");
     let repo_details = match sqlx::query!(
-        "SELECT r.* FROM Repository r INNER JOIN Team t ON t.id = r.teamId INNER JOIN User u ON u.id = r.userId LEFT JOIN ApiKey a ON a.userId = u.id OR a.teamId = t.id WHERE r.id= ?",
+        "SELECT r.*, a.key  FROM Repository r INNER JOIN Team t ON t.id = r.teamId INNER JOIN User u ON u.id = r.userId LEFT JOIN ApiKey a ON a.userId = u.id OR a.teamId = t.id WHERE r.id= ?",
         gh_payload.installation.id
     )
     .fetch_one(&db_pool)
