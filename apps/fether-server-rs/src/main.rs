23389a4f9f826512a98cf7566e1c8a88693b5a37
0xFloop <95703085+0xFloop@users.noreply.github.com>
2024-04-10 15:34:15 -0600

    testing

--- apps/fether-server-rs/src/main.rs
+++ apps/fether-server-rs/src/main.rs
@@ -561,7 +561,10 @@ async fn github_payload_handler(
                         let deploy_factory =
                             ContractFactory::new(abi, contract_data.clone(), provider);
 
-                        let ummm = deploy_factory.deploy("(33, \"hello\")".to_string());
+                        let ummm = deploy_factory.deploy_tokens(vec![
+                            ethers::abi::Token::Uint(U256::from(98)),
+                            ethers::abi::Token::String("hello".to_string()),
+                        ]);
 
                         println!("{:?}", ummm);
 
