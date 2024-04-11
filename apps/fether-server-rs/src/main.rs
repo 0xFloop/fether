b2f66a247e6a9672450ac7243d9f41ac1bf272fd
0xFloop <95703085+0xFloop@users.noreply.github.com>
2024-04-11 11:31:51 -0600

    testing

--- apps/fether-server-rs/src/main.rs
+++ apps/fether-server-rs/src/main.rs
@@ -602,8 +602,8 @@ async fn github_payload_handler(
                         };
                         println!("between");
                         let hash = tx_res.unwrap().tx_hash();
-                        let tx = provider.get_transaction(hash).await;
-                        //sleep(time::Duration::from_secs(6));
+                        //let tx = provider.get_transaction(hash).await;
+                        sleep(time::Duration::from_secs(6));
                         let receipt = provider.get_transaction_receipt(hash).await;
                         println!("reciept: {receipt:?}");
                         //update repository in db with new contract address and lastDeployed time
